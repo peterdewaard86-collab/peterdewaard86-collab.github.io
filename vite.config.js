22bba4627cac79f03720d6d933fd8102d76809f8
peterdewaard86-collab <peterdewaard86@gmail.com>
2026-08-17 14:54:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,10 +19,10 @@ export default defineConfig({
         orientation: "portrait",
         start_url: "/",
         scope: "/",
-        icons: [
-          { src: "https://bullss-bearss.netlify.app/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "https://bullss-bearss.netlify.app/icon-512.png", sizes: "512x512", type: "image/png" },
-          { src: "https://bullss-bearss.netlify.app/icon-maskable-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
+       icons: [
+          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
+          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
+          { src: "icon-maskable-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
         ]
       },
       workbox: {
